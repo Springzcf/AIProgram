eeca1b1fd539b059586b972fe8c1872eead7c7f5
Springzcf <506484864@qq.com>
2017-12-15 16:37:10 +0800

    开始学习tensorflow

--- ai_tensorflow/MNISTDemo/dataSet/downloadData.py
+++ ai_tensorflow/MNISTDemo/dataSet/downloadData.py
@@ -5,4 +5,4 @@ download data for Mnist
 '''
 import input_data as input_data
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("./MNIST_data/", one_hot=True)
