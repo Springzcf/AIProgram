229b6780da9cefe71486923a7e6cba4efcc59a63
Springzcf <506484864@qq.com>
2017-12-18 16:44:08 +0800

    开始学习tensorflow

--- ai_tensorflow/MNISTDemo/dataSet/downloadData.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on 2017年12月14日
-download data for Mnist
-@author: zhangcf17306
-'''
-import input_data as input_data
-
-mnist = input_data.read_data_sets("./MNIST_data/", one_hot=True)
--- ai_tensorflow/MnistDemo1.py
+++ ai_tensorflow/MnistDemo1.py
@@ -1,15 +1,16 @@
 '''
-Created on 2017年12月15日
-
-@author: zhangcf17306
+主要
 '''
 
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
 
 
 # 占位符placeholder，我们在TensorFlow运行计算时输入这个值
 # placeholder params:类型,二维张量[任意长度,]
-x = tf.placeholder("float", [None, 784])
+x = tf.placeholder(tf.float32, [None, 784])
 
 # Variable代表一个可修改的张量
 W = tf.Variable(tf.zeros([784, 10]))
@@ -19,20 +20,24 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 #
-y_ = tf.placeholder("float", [None, 10])
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = -tf.reduce_sum(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
 # 梯度下降
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 
-init = tf.initialize_all_variables()
+sess = tf.InteractiveSession()
 
-sess = tf.Session()
-sess.run(init)
+tf.global_variables_initializer().run()
+#
+# for _ in range(1000):
+#     batch_xs, batch_ys = mnist.train.next_batch(100)
+#     sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
 
-for i in range(1000):
-    batch_xs, batch_ys = data_set.next_batch(100)
-    sess.run(train_step, feed_dict={})
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
 
 
